141034fb965fbf8b628b1a2c356f9c990c328468
marcellbarsony <barsonymarcell@protonmail.com>
2023-10-22 21:52:42 +0200

    Added mirrorlist update

---
+++ mirrorlist.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Author: Marcell Barsony
+Date  : 05/13/23
+Desc  : Refresh mirrorlist
+"""
+
+
+import subprocess
+
+
+def mirrorlist():
+    cmd = "sudo \
+    reflector \
+    --latest 25 \
+    --protocol https \
+    --connection-timeout 5 \
+    --sort rate \
+    --save /etc/pacman.d/mirrorlist"
+    try:
+        print("Updating Pacman mirrorlist...")
+        subprocess.run(cmd, shell=True, check=True, stdout=subprocess.DEVNULL)
+        print("[+] PACMAN: Mirrorlist")
+    except subprocess.CalledProcessError as err:
+        print(f"[-] PACMAN: Mirrorlist {err}")
+
+
+if __name__ == "__main__":
+    mirrorlist()
